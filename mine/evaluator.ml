98d2a0716f819d1c304a19b988c58b6eb37548fd
Aeon <kheireddine.ounnoughi@etu.sorbonne-universite.fr>
2025-03-23 02:16:50 +0100

    everything works except ASTArrow , added few tests

--- mine/evaluator.ml
+++ mine/evaluator.ml
@@ -77,15 +77,28 @@ let get_val ident env =
   | None -> failwith ("No value for identifier: " ^ ident)
 ;;
 
+
+
+let rec add_variables_to_env (args : string list) (values : value list) ( env: environment)=
+  let rec aux_add args vals res_env = 
+    match args,vals with
+    | [], [] -> res_env
+    | id :: rest_args , value::rest_values -> 
+      let res = StringMap.add id value res_env in
+      aux_add rest_args rest_values res
+    | _ -> failwith "Arguments mismatch in function"
+    in
+    aux_add args values env;;
+
 let prim1 arg = not_op_func arg;;
 let prim2 ident arg1 arg2 env = 
   match ident with
-  | `eq -> eq_func [arg1;arg2]
-  | `lt -> lt_func [arg1;arg2]
-  | `add -> add_func [arg1;arg2]
-  | `sub -> sub_func [arg1;arg2]
-  | `mul -> mul_func [arg1;arg2]
-  | `div -> div_func [arg1;arg2]
+  | "eq" -> eq_func [arg1;arg2]
+  | "lt" -> lt_func [arg1;arg2]
+  | "add" -> add_func [arg1;arg2]
+  | "sub" -> sub_func [arg1;arg2]
+  | "mul" -> mul_func [arg1;arg2]
+  | "div" -> div_func [arg1;arg2]
   | _ -> failwith "Invalid function name"
 ;;
 
@@ -97,6 +110,22 @@ let wrap ident args env =
     prim2 ident arg1 arg2 env
   | _ -> failwith " not a prim function";;
 
+  let find_key_for_value map value =
+    StringMap.fold (fun key v result -> 
+      if v = value && result = None then Some key else result
+    ) map None
+    ;;
+
+let eval_lambda params expr env =
+  InF(expr, params, env);;
+
+let args_list_tostring arglist = 
+  let rec aux_transform arg_list res = 
+    match arg_list with
+    | [] -> List.rev res
+    | ASTArg(ident,_)::td -> aux_transform td (ident::res)
+  in 
+  aux_transform arglist [];; 
 (* expression evaluator*)
 let rec eval_expr e env = 
   match e with
@@ -117,8 +146,59 @@ let rec eval_expr e env =
       eval_expr body env
     else
       eval_expr alternate env
+  | ASTApp(expr , exprs) -> eval_app expr exprs env
+  | ASTLambda(args, e) ->
+    let new_args = args_list_tostring args in
+    eval_lambda new_args e env
   | _ ->  failwith " Unknown expression"
-;;
+
+and eval_exprs es env = 
+  match es with 
+  | [] -> []
+  | e::exprs -> (eval_expr e env)::eval_exprs exprs env
+
+and eval_app expr expressions env =
+  let new_v = eval_expr expr env in 
+  let arg_values = eval_exprs expressions env in 
+  match new_v with
+  | InP id ->
+    if List.length expressions > 2 then
+      failwith "Not the right args for the inP, no more than 2"
+    else
+      wrap id arg_values env  (* Reversing the list to maintain original order *)
+
+  | InF (body, params, env_fun) ->
+      let new_env = add_variables_to_env params arg_values env_fun in
+      eval_expr body new_env 
+  | InFR (body, ident, params, env_fun) -> 
+        let rec_env = StringMap.add ident (InFR (body, ident, params, env)) env_fun in
+        let new_env = add_variables_to_env params arg_values rec_env in
+        eval_expr body new_env
+  | _ -> failwith"Not an existing app"
+  ;;
+
+
+let eval_def def env =
+  match def with
+  | ASTConst (ident, ttype, expr) ->
+      let new_env = StringMap.add ident (eval_expr expr env) env in  (* Correctly update env *)
+      new_env 
+
+  | ASTFun (ident, ttype, arg_list, expr) ->
+      let new_args = args_list_tostring arg_list in 
+      let new_val = InF(expr, new_args, env) in
+      let new_env = StringMap.add ident new_val env in 
+      new_env
+  
+  | ASTFunRec (ident, ttype, arg_list, expr) ->
+    let new_args = args_list_tostring arg_list in 
+    let new_env = StringMap.add ident (InFR (expr, ident, new_args, env)) env in
+    new_env
+  ;;
+
+
+
+
 
 let print_value value =
   match value with
@@ -130,15 +210,18 @@ let print_output lst =
 ;;
 
 
+
 let eval_stat s env output = 
   match s with 
   | ASTEcho e -> (eval_expr e env )::output
 ;;
 
-let eval_cmd cmd env output = 
+let rec eval_cmd cmd env output = 
   match cmd with
   | ASTStat stat -> eval_stat stat env output
-  | ASTDef (_, _) -> output
+  | ASTDef (def, cmds) -> 
+    let new_env = eval_def def env in
+    eval_cmd cmds new_env output
 ;;
 
 let eval_prog p env = 
